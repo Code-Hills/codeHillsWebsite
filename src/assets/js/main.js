a9dee5e3926760e7d0b7b819babecb9574be5da7
Prophete Isingizwe <prophete.isingizwe@codeland.it>
2024-01-24 04:56:26 +0200

    Added landing page

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,25 +1,36 @@
 // methods
 
 
-/* initialize hero banner slides 
+/* initialize hero banner slides
 *
 * @return {void}
 */
-function initHeroBannerSlides() {
-    var heroSwiper = new Swiper('.hero-banner-swiper', {
-        loop: true,
-        slidesPerView: 1,
-        autoplay: {
-            delay: 5000,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-    });
+function initSlideshow() {
+    let current = 0;
+    const slides = document.querySelectorAll("#slideshow .slide");
+
+    setInterval(() => {
+
+        slides[current].style.opacity = 0;
+        current = (current + 1) % slides.length;
+        slides[current].style.opacity = 1;
+    }, 5000);
+}
+
+function initDotsAnimation() {
+    const dots = document.getElementById('dots');
+    const text = ["", ".", "..", "..."];
+    let index = 0;
+
+    setInterval(() => {
+        dots.textContent = text[index % text.length];
+        index++;
+    }, 700);
+
 }
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    initHeroBannerSlides();
+    initSlideshow();
+    initDotsAnimation();
 });

05b449d790c1253a2f0a092889924163d11315b1
Niyonzi Raufu <niyonzi.raufu@codeland.it>
2024-01-26 16:23:51 +0200

    improve landing page

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,25 +1,83 @@
-// methods
+// scripts.js
 
+function appendSlideShowImages() {
+    const imagesArray = [];
 
-/* initialize hero banner slides 
-*
-* @return {void}
-*/
-function initHeroBannerSlides() {
-    var heroSwiper = new Swiper('.hero-banner-swiper', {
+    const imagesFilesNames = [
+        "codehills-img_1.jpg",
+        "codehills-img_3.jpg",
+        "codehills-img_4.jpg",
+        "codehills-img_6.jpg",
+        "codehills-img_7.jpg",
+        "codehills-img_8.jpg",
+        "codehills-img_9.jpg",
+        "codehills-img_10.jpg",
+        "codehills-img_11.jpg",
+        "codehills-img_12.jpg",
+        "codehills-img_13.jpg",
+    ];
+
+    const imagesPath = "assets/images/";
+
+    imagesFilesNames.forEach((imageFileName) => {
+        const image = new Image();
+        image.src = imagesPath + imageFileName;
+        image.alt = "Codehills-" + imageFileName;
+        image.classList.add("coming-soon__slideShow--slide-image");
+        imagesArray.push(image);
+    });
+
+    const swiperWrapper = document.querySelector(".swiper-wrapper");
+    imagesArray.forEach((image) => {
+        const swiperSlide = document.createElement("div");
+        swiperSlide.classList.add(
+            "coming-soon__slideShow--slide-image",
+            "swiper-slide"
+        );
+        swiperSlide.appendChild(image);
+        swiperWrapper.appendChild(swiperSlide);
+    });
+}
+
+function initSlideShow() {
+    const swiper = new Swiper(".swiper", {
         loop: true,
-        slidesPerView: 1,
+        effect: "fade",
         autoplay: {
             delay: 5000,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
         },
     });
 }
 
+function initDotsAnimation() {
+    const dots = document.getElementById("dots");
+    const text = ["", ".", "..", "..."];
+    let index = 0;
 
-document.addEventListener('DOMContentLoaded', function () {
-    initHeroBannerSlides();
+    setInterval(() => {
+        dots.textContent = text[index % text.length];
+        index++;
+    }, 700);
+}
+
+function getScreenWidth() {
+    document.documentElement.style.setProperty(
+        "--screen-width",
+        `${window.innerWidth}px`
+    );
+}
+
+function isLandScape() {
+    return window.matchMedia("(orientation: landscape)").matches;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    appendSlideShowImages();
+    initSlideShow();
+    initDotsAnimation();
+    getScreenWidth();
+});
+
+window.addEventListener("resize", () => {
+    getScreenWidth();
 });
